23a83627467a91c4f535ffa421ff269db1e3e28e
Purinat33 <78997216+Purinat33@users.noreply.github.com>
2025-09-22 16:01:20 +0700

    Revert changes to data sources

--- services/data_sources.py
+++ services/data_sources.py
@@ -106,7 +106,7 @@ def fetch_from_sacct(start_date: str, end_date: str, username: str | None = None
         "ReqTRES,AllocTRES,AveRSS,MaxRSS,TRESUsageInTot,TRESUsageOutTot,End,State,"
         "ExitCode,DerivedExitCode,"
         "ConsumedEnergyRaw,ConsumedEnergy,"
-        "NodeList,AllocNodes,Account,WCKey,Comment,WorkDir"
+        "NodeList,AllocNodes"
     ]
     if username:
         cmd += ["-u", username]
