b2a8fc33c51f78001232edd254860ec1631e021d
Purinat33 <78997216+Purinat33@users.noreply.github.com>
2025-09-25 23:06:01 +0700

    Update org info for invoice rendering

--- services/org_info.py
+++ services/org_info.py
@@ -4,15 +4,15 @@ import os
 def ORG_INFO():
     return {
         "name": os.getenv("ORG_NAME", "MU AI Center"),
-        "address_line1": os.getenv("ORG_ADDR1", "999 Putthamonton"),
-        "address_line2": os.getenv("ORG_ADDR2", ""),
+        "address_line1": os.getenv("ORG_ADDR1", "999 Phutthamonthon Sai 4 Road"),
+        "address_line2": os.getenv("ORG_ADDR2", "Salaya"),
         "city": os.getenv("ORG_CITY", "Nakhon Pathom"),
-        "postcode": os.getenv("ORG_POST", "10110"),
+        "postcode": os.getenv("ORG_POST", "73170"),
         "country": os.getenv("ORG_COUNTRY", "Thailand"),
         "tax_id": os.getenv("ORG_TAX_ID", ""),
-        "email": os.getenv("ORG_EMAIL", "billing@example.com"),
-        "phone": os.getenv("ORG_PHONE", "+66 2 000 0000"),
-        "bank_name": os.getenv("ORG_BANK_NAME", "Bangkok Bank"),
+        "email": os.getenv("ORG_EMAIL", "opwww@mahidol.ac.th"),
+        "phone": os.getenv("ORG_PHONE", "+66 (0) 2849-6000"),
+        "bank_name": os.getenv("ORG_BANK_NAME", "Siam Commercial Bank"),
         "bank_acct": os.getenv("ORG_BANK_ACCT", "123-4-56789-0"),
         "bank_holder": os.getenv("ORG_BANK_HOLDER", "Mahidol University"),
     }
